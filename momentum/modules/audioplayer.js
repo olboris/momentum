0d9ace9a77b626bc74c8bf00f9ebcc35c3f148a3
Olga Babushkina <boriskinaos@mail.ru>
2021-10-21 17:12:26 +0300

    feat: add progress-bar, time field at player

--- momentum/modules/audioplayer.js
+++ momentum/modules/audioplayer.js
@@ -5,11 +5,19 @@ const playListContainer = document.querySelector('.play-list');
 const playButton = document.querySelector('.play');
 const playNextButton = document.querySelector('.play-next');
 const playPrevButton = document.querySelector('.play-prev');
+const songTitle = document.querySelector('.audio-title');
+const currentSongDuration = document.querySelector('.time-length');
+const progressBar = document.querySelector('.progress-bar');
 
 let isPlay = false;
 let playNum = 0;
 let index = 0;
-console.log(playList[playNum])
+
+songTitle.textContent = playList[playNum].title;
+currentSongDuration.textContent = playList[playNum].duration;
+progressBar.max = playList[playNum].duration;
+audio.src = playList[playNum].src;
+songTitle.textContent = playList[playNum].title;
 
 playList.forEach(el => {
     const li = document.createElement('li');
@@ -25,15 +33,18 @@ playList.forEach(el => {
 
 function playAudio() {
     if(!isPlay) {
-        audio.src = playList[playNum].src
-        audio.currentTime = 0;
+        //audio.src = playList[playNum].src;
+        //audio.currentTime = progressBar.value;
+        // songTitle.textContent = playList[playNum].title;
+        // currentSongDuration.textContent = playList[playNum].duration;
         audio.play();
         playButton.classList.add('pause');
+        document.querySelector('.item-active').classList.add('item-active-pause');
         isPlay = true;
     } else {
         audio.pause();
-        console.log("!!!");
         playButton.classList.remove('pause');
+        document.querySelector('.item-active').classList.remove('item-active-pause');
         isPlay = false;
     }
 }
@@ -46,9 +57,8 @@ export function playNext() {
         playNum = 0;
         document.getElementById(playList.length-1).classList.remove('item-active');
     };
-    isPlay = !isPlay ? true : false;
+    updateSong();
     playAudio();
-    document.getElementById(playNum).classList.add('item-active');
 }
 
 export function playPrev() {
@@ -59,11 +69,61 @@ export function playPrev() {
         playNum = playList.length-1;
         document.getElementById(0).classList.remove('item-active');
     };
-    isPlay = !isPlay ? true : false;
+    updateSong();
     playAudio();
-    document.getElementById(playNum).classList.add('item-active');
 }
 
+function updateSong() {
+    isPlay = !isPlay ? true : false;
+    audio.src = playList[playNum].src;
+    songTitle.textContent = playList[playNum].title;
+    currentSongDuration.textContent = playList[playNum].duration;
+    audio.currentTime = 0;
+    document.getElementById(playNum).classList.add('item-active');
+    updateProgressValue();
+}
+
+function updateProgressValue() {
+    const percent = (audio.currentTime / formatDuration(playList[playNum].duration)) * 100;
+    progressBar.value = percent;
+    progressBar.style.background = `linear-gradient(to right, #C5B358 0%, #C5B358 ${percent}%, #fff ${percent}%, #fff 100%)`;
+    document.querySelector('.time-current').innerHTML = (formatTime(Math.floor(audio.currentTime)));
+};
+
+function formatDuration(duration) {
+    let arrTime = duration.split(':');
+    let sec = +arrTime[0]*60 + +arrTime[1];
+    return sec;
+};
+
+function formatTime(seconds) {
+    let min = Math.floor((seconds / 60));
+    let sec = Math.floor(seconds - (min * 60));
+    if (sec < 10){ 
+        sec  = `0${sec}`;
+    };
+    return `${min}:${sec}`;
+};
+
+function handleRangeUpdate() {
+    audio.currentTime = this.value / 100 * formatDuration(playList[playNum].duration);
+    this.style.background = `linear-gradient(to right, #C5B358 0%, #C5B358 ${this.value}%, #fff ${this.value}%, #fff 100%)`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progressBar.offsetWidth) * formatDuration(playList[playNum].duration);
+    audio.currentTime = scrubTime;
+}
+
+audio.addEventListener('ended', playNext);
+audio.addEventListener('timeupdate', updateProgressValue);
 playButton.addEventListener('click', playAudio);
 playNextButton.addEventListener('click', playNext);
 playPrevButton.addEventListener('click', playPrev);
+progressBar.addEventListener('change', handleRangeUpdate);
+progressBar.addEventListener('mousemove', handleRangeUpdate);
+progressBar.addEventListener('loadeddata', handleRangeUpdate);
+progressBar.addEventListener('click', scrub);
+progressBar.addEventListener('mousemove', (e) => mousedown && scrub(e));
+//progressBar.addEventListener('mousedown', () => mousedown = true);
+//progressBar.addEventListener('mouseup', () => mousedown = false);
--- momentum/modules/time.js
+++ momentum/modules/time.js
@@ -8,7 +8,7 @@ export default function showTime() {
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime;
     showDate(date);
-    setInterval(showTime, 1000);
+    setTimeout(showTime, 1000);
     showGreeting();
 }
 
