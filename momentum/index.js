61c72300790809430500bd3dbf5838b4ae0c56a4
Olga Babushkina <boriskinaos@mail.ru>
2021-10-25 14:09:25 +0300

    feat: add links block

--- momentum/index.js
+++ momentum/index.js
@@ -2,6 +2,7 @@ import showTime from './modules/time.js';
 import {setBg, getSlideNext, getSlidePrev, randomNum} from './modules/slider.js';
 import getWeather from './modules/weather.js';
 import getQuotes from './modules/quotes.js';
+import { links, createLink, linksForm, openForm, closeForm, newLinkButton, closeFormButton, formSubmit, linksContainer } from './modules/links.js';
 
 const name = document.querySelector('.name');
 const city = document.querySelector('.city');
@@ -11,9 +12,16 @@ const changeQuote = document.querySelector('.change-quote');
 const langRuRadio = document.querySelector('#lang-ru');
 const langEnRadio = document.querySelector('#lang-en');
 let lang = localStorage.getItem('lang') || 'en';
+const defaultCity = { 'en': 'Minsk', 'ru':'Минск'};
+const linkSubtitleContent = { 'en': 'New link', 'ru': 'Новая ссылка'};
+const linkTitleContent = { 'en': 'Links', 'ru':'Ссылки'};
+const linksTitle = document.querySelector('.links-title');
 
 langRuRadio.checked = lang === 'ru' ? true : false;
-langEnRadio.checked = lang === 'en' ? true : false;  
+langEnRadio.checked = lang === 'en' ? true : false;
+
+newLinkButton.textContent = linkSubtitleContent[lang];
+linksTitle.textContent = linkTitleContent[lang];
 
 function setLocalStorage() {
     localStorage.setItem('name', name.value);
@@ -28,7 +36,7 @@ function getLocalStorage() {
     }
     if(localStorage.getItem('city')){
         city.value = localStorage.getItem('city');
-    } else city.value = "Minsk";
+    } else city.value = defaultCity[lang];
 
     if(localStorage.getItem('lang')){
         lang = localStorage.getItem('lang');
@@ -37,14 +45,21 @@ function getLocalStorage() {
 window.addEventListener('load', getLocalStorage);
 
 showTime(lang, name);
-getWeather(localStorage.getItem('city') || "Minsk", lang);
+getWeather(localStorage.getItem('city') || defaultCity[lang], lang);
 getQuotes(lang);
 setBg(randomNum);
+links.forEach(el => {
+    const link = createLink(el);
+    const placeElement = link.generateLink();
+    linksContainer.append(placeElement);
+  });
+
 slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getSlidePrev);
 city.addEventListener('change', ()=> {getWeather(city.value)});
 changeQuote.addEventListener('click', getQuotes);
 langRuRadio.addEventListener('click', () => {lang = 'ru'; console.log(lang); window.location.reload()});
 langEnRadio.addEventListener('click', () => {lang = 'en'; console.log(lang); window.location.reload()});
-console.log(localStorage.getItem('lang'));
-console.log(langEnRadio.checked);
+linksForm.addEventListener('submit', (evt) => { formSubmit(evt) });
+newLinkButton.addEventListener('click', openForm);
+closeFormButton.addEventListener('click', closeForm);
